2369133b8d591003ceca4aa61f51a949b3680104
Student Nick Borle <nickb.ubc@gmail.com>
2017-03-25 14:28:10 -0700

    Updated SQL to v2
    After our meeting with Joan, a few requirments wer chanegd. One of those
    requirments was the database.

--- sql/createDB.sql
+++ sql/createDB.sql
@@ -3,60 +3,21 @@ CREATE TABLE Event (
 	date DATE,
 	level int,
 	location VARCHAR(255),
-	teller VARCHAR(255),
-	impromptuTopic VARCHAR(255),
-	timekeeper1 VARCHAR(255),
-	timekeeper2 VARCHAR(255),
-	judge1 VARCHAR(255),
-	judge2 VARCHAR(255),
-	judge3 VARCHAR(255),
 	PRIMARY KEY (id)
 	);
 
-CREATE TABLE Topic (
-	name VARCHAR(255) NOT NULL,
-	eventID int,
-	PRIMARY KEY (name),
-	FOREIGN KEY (eventID) REFERENCES Event(id)
-);
-
 CREATE TABLE Speaker (
 	eventID int NOT NULL,
-	id int NOT NULL,
-	name VARCHAR(255),
-	finalScore DOUBLE(4,3),
-	preparedTime TIME,
-	impromptuTime TIME,
-	topicName VARCHAR(255),
-	PRIMARY KEY (eventID, id),
-	FOREIGN KEY (eventID) REFERENCES Event(id),
-	FOREIGN KEY (topicName) REFERENCES Topic(name)
-);
-
-CREATE TABLE Scoresheet (
-	id int NOT NULL,
-	row1 int,
-	row2 int,
-	row3 int,
-	row4 int,
-	row5 int,
-	row6 int,
-	row7 int,
-	row8 int,
-	row9 int,
-	row10 int,
-	row11 int,
-	row12 int,
-	row13 int,
-	row14 int,
-	eventID int,
-	speakerID int,
-	PRIMARY KEY (id),
-	FOREIGN KEY (eventID, speakerID) REFERENCES Speaker(eventID, id)
+	speaker VARCHAR(255) NOT NULL,
+	finalScore DOUBLE,
+	squad VARCHAR(255),
+	PRIMARY KEY (eventID, speaker),
+	FOREIGN KEY (eventID) REFERENCES Event(id)
 );
 
 CREATE TABLE user (
 	username VARCHAR(255) NOT NULL,
 	password VARCHAR(255) NOT NULL,
+	permissions boolean,
 	PRIMARY KEY (username)
 );
